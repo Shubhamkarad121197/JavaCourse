5f45b680be5284aeef990083bd2bb28284470890
shubham karad <shubham.karad12@gmail.com>
2026-03-21 22:54:30 +0530

    Basic of java

---
+++ Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+    public static void main(String a[]){
+        int num1=5;
+        int num2=6;
+        int result=num1+num2;
+        System.out.println(result);
+    }
+}
