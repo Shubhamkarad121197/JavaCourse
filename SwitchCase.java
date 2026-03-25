2cca22f6ecce7b70b56b9cffe5f8b92a35cc23fb
shubham karad <shubham.karad12@gmail.com>
2026-03-25 23:08:11 +0530

    SwitchCase

---
+++ SwitchCase.java
@@ -0,0 +1,15 @@
+public class SwitchCase {
+    public static void main(String args[]){
+        int n=3;
+        switch(n){
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thrusday");
+            case 6 -> System.out.println("Friday");
+             case 7 -> System.out.println("Saturday");
+            default -> System.out.println("Sunday");
+
+        }
+    }
+}
