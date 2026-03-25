3fa209913ce007c9fe68da84256f48619df961d1
shubham karad <shubham.karad12@gmail.com>
2026-03-25 22:45:28 +0530

    ifelse change

--- IfElse.java
+++ IfElse.java
@@ -1,16 +1,19 @@
-public class IfElse {
-    public static void main(String[] args) {
 
-        int marks = 75;
-
-        if (marks >= 90) {
+public class IfElse{
+    public static void main(String a[]){
+        int marks=65;
+        if(marks>=75){
             System.out.println("Grade A");
-        } else if (marks >= 60) {
+        }
+        else if(marks>=60 & marks<75){
             System.out.println("Grade B");
-        } else if (marks >= 40) {
+        }
+        else if(marks>=40 & marks<60){
             System.out.println("Grade C");
-        } else {
+        }
+        else{
             System.out.println("Fail");
         }
+
     }
 }
