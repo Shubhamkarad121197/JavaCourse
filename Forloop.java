581f8cc4c2cc681bc6c315972588adcc284d46a5
shubham karad <shubham.karad12@gmail.com>
2026-03-26 22:11:58 +0530

    forloop Star pattern

---
+++ Forloop.java
@@ -0,0 +1,8 @@
+public class Forloop {
+    public static void main(String[] args) {
+        int i = 2;
+        for (i = 0; i < 20; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+}
