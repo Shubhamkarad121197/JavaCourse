f9af2facc0437a1568bed450a0f4d1edf86ffa2e
shubham karad <shubham.karad12@gmail.com>
2026-04-25 00:10:37 +0530

    Real world thread example

---
+++ Threads/RealWorld.java
@@ -0,0 +1,17 @@
+class Task implements  Runnable{
+    public void run(){
+        System.out.println(Thread.currentThread().getName());
+    }
+}
+
+public class RealWorld {
+     public static void main(String[] args) {
+
+        Thread t1 = new Thread(new Task());
+        Thread t2 = new Thread(new Task());
+
+        t1.start();
+        t2.start();
+    }
+    
+}
